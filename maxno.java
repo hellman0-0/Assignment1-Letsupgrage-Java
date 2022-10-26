bc65ff6372681d407b3b8b31e7573974bb7886be
hellman0-0 <77567765+hellman0-0@users.noreply.github.com>
2022-10-26 06:24:34 +0530

    Assignment uploaded

---
+++ maxno.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class maxno
+{
+    /**
+     * @param args
+     */
+    public static void main(String args[])
+    {
+       System.out.println("taking input from user");
+       Scanner sc= new Scanner(System.in);
+       System.out.println("Enter Number 1");
+       int a=sc.nextInt();
+       System.out.println("Enter Number 2");
+       int b=sc.nextInt();
+       System.out.println("Enter Number 3");
+       int c=sc.nextInt();
+       int m=a;
+       if(b>m)
+       m=b;
+       if(c>m)
+       m=c;
+       System.out.println("Maximum No is");
+       System.out.println(m);
+       return; 
+    }
+}
